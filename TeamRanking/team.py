fae3c26153ff619206e9d3bac809342d9c7e9e23
Andrew Kanarek <akanarek@umich.edu>
2019-08-26 19:31:52 -0400

    fix error with teamranking

--- TeamRanking/team.py
+++ TeamRanking/team.py
@@ -3,7 +3,8 @@ class Team:
 	def __init__(self, name):
 		self.name = name
 		self.score = .25
-		self.games
+		self.games_won = []
+		self.games_lost = []
 
 	def addWin(self, opponent):
 		self.games_won.append(opponent)
